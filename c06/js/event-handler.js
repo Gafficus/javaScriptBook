de8b6acb75ca77153f1e6e6a8d3083bff0955c8a
Nathan A. Gaffney <nagaffne@csis.svsu.edu>
2017-10-25 12:41:36 -0400

    Assignment completed

--- c06/js/event-handler.js
+++ c06/js/event-handler.js
@@ -1,5 +1,6 @@
 function checkUsername() {                            // Declare function
-  var elMsg = document.getElementById('feedback');    // Get feedback element
+  var str= this.id;
+  var elMsg = document.getElementById('feedback' + str[str.length -1]);    // Get feedback element
   if (this.value.length < 5) {                        // If username too short
     elMsg.textContent = 'Username must be 5 characters or more';  // Set msg
   } else {                                            // Otherwise
@@ -7,5 +8,13 @@ function checkUsername() {                            // Declare function
   }
 }
 
-var elUsername = document.getElementById('username'); // Get username input
+var elUsername = document.getElementById('username0'); // Get username input
 elUsername.onblur = checkUsername;  // When it loses focus call checkuserName()
+var elUsername1 = document.getElementById('username1'); // Get username input
+elUsername1.onblur = checkUsername;  // When it loses focus call checkuserName()
+var elUsername2 = document.getElementById('username2'); // Get username input
+elUsername2.onblur = checkUsername;  // When it loses focus call checkuserName()
+var elUsername3 = document.getElementById('username3'); // Get username input
+elUsername3.onblur = checkUsername;  // When it loses focus call checkuserName()
+var elUsername4 = document.getElementById('username4'); // Get username input
+elUsername4.onblur = checkUsername;  // When it loses focus call checkuserName()
--- c06/js/event-listener.js
+++ c06/js/event-listener.js
@@ -1,12 +1,52 @@
 function checkUsername() {                             // Declare function
-  var elMsg = document.getElementById('feedback');     // Get feedback element
+  var str = this.id;
+  var zippydee = this.value[0];
+  var elMsg = document.getElementById('feedback' + str[str.length-1]);     // Get feedback element
   if (this.value.length < 5) {                         // If username too short
     elMsg.textContent = 'Username must be 5 characters or more'; // Set msg
   } else {                                             // Otherwise
     elMsg.textContent = '';                            // Clear msg
   }
+
+  var check = zippydee.toLowerCase()
+  var flag = 0;
+  switch(str[str.length-1]) {
+    case "0":
+        if(check == "a" || check == "b" || check == "c"){
+          flag = 1;
+        }
+        break;
+    case "1":
+        if(check == "d" || check == "e" || check == "f"|| check == "g" || check == "h"){
+          flag = 1;
+        }
+        break;
+    case "2":
+        if(check == "i" || check == "j" || check == "k"|| check == "l" || check == "m"){
+          flag = 1;
+        }
+        break;
+    case "3":
+        if(check == "n" || check == "o" || check == "p"){
+          flag = 1;
+        }
+        break;
+    case "4":
+        if(check == "q" || check == "r" || check == "s"|| check == "t" || check == "u" || check == "v"|| check == "w" || check == "x" || check == "y"|| check == "z"){
+          flag = 1;
+        }
+        break;
+    default:
+        flag=0;
+  }  
+  if(flag==0){elMsg.textContent='Username must start with correct letter'};
+}
+  
+for (i = 0; i < 5; i++) { 
+    var elUsername = document.getElementById('username' + i);
+	elUsername.addEventListener('blur', checkUsername, false);
 }
 
-var elUsername = document.getElementById('username');  // Get username input
+//var elUsername = document.getElementById('username0');  // Get username input
 // When it loses focus call checkUsername()
-elUsername.addEventListener('blur', checkUsername, false);
+//elUsername.addEventListener('blur', checkUsername, false);
